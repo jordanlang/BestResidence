03337f6a54f8691d8cba2a0d79c2910b2b2b6f9e
x0nePeace <contact@jordanlang.fr>
2016-05-04 11:19:38 +0200

    projet version 1.0

--- BestResidence/mainwindow.cpp
+++ BestResidence/mainwindow.cpp
@@ -31,9 +31,9 @@ MainWindow::MainWindow(QWidget *parent) :
     ui->tableBienLoue->setSelectionBehavior(QAbstractItemView::SelectRows);
     ui->tableBienVendu->setSelectionBehavior(QAbstractItemView::SelectRows);
     ui->tableClient->setSelectionBehavior(QAbstractItemView::SelectRows);
-    int w = ui->l_icon->width();
-    int h= ui->l_icon->height()+40;
-    QPixmap *pixmap_img2 = new QPixmap("../../../../BestResidence/Img/icon.jpg");
+    int w = ui->l_icon->width()+210;
+    int h= ui->l_icon->height()+210;
+    QPixmap *pixmap_img2 = new QPixmap("../../../../BestResidence/Img/icon.png");
     ui->l_icon->setPixmap((*pixmap_img2).scaled(w,h,Qt::KeepAspectRatio));
     readXmlFile();
     set_aff_annonces_vente();
